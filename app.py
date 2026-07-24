e1567191ab9d8f770564ff6229eb70f8251da182
Abdulhye11 <abdulhye4011@gmail.com>
2026-07-24 11:46:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,53 @@ It must **not** be used as a substitute for professional medical advice.
 """)
 
 st.divider()
+# ==========================================
+# PATIENT INFORMATION
+# ==========================================
+
+st.header("👤 Patient Information")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    age = st.number_input(
+        "Age",
+        min_value=1,
+        max_value=100,
+        value=40
+    )
+
+with col2:
+    gender = st.selectbox(
+        "Gender",
+        ["Male", "Female"]
+    )
+
+gender_value = 1 if gender == "Male" else 0
+# ==========================================
+# CBC PARAMETERS
+# ==========================================
+
+st.header("🩸 Complete Blood Count (CBC)")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    WBC = st.number_input("WBC", value=7.0)
+    RBC = st.number_input("RBC", value=4.8)
+    Hemoglobin = st.number_input("Hemoglobin", value=14.5)
+    Hematocrit = st.number_input("Hematocrit", value=42.0)
+    Platelets = st.number_input("Platelets", value=250)
+
+with col2:
+    MCV = st.number_input("MCV", value=90.0)
+    MCH = st.number_input("MCH", value=30.0)
+    MCHC = st.number_input("MCHC", value=33.0)
+    RDW = st.number_input("RDW", value=13.0)
+
+with col3:
+    Neutrophils = st.number_input("Neutrophils", value=60.0)
+    Lymphocytes = st.number_input("Lymphocytes", value=30.0)
+    Monocytes = st.number_input("Monocytes", value=5.0)
+    Eosinophils = st.number_input("Eosinophils", value=3.0)
+    Basophils = st.number_input("Basophils", value=1.0)
